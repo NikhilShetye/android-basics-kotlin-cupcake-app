7b865cc7ce2c8965354be097d3d2d912ae99eb73
nikhi <nikhilshetye26@gmail.com>
2021-04-18 10:11:10 +0530

    logic for setting user selected values

--- app/src/main/java/com/example/cupcake/StartFragment.kt
+++ app/src/main/java/com/example/cupcake/StartFragment.kt
@@ -20,13 +20,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.cupcake.databinding.FragmentStartBinding
+import com.example.cupcake.model.OrderViewModel
 
 /**
  * This is the first screen of the Cupcake app. The user can choose how many cupcakes to order.
  */
 class StartFragment : Fragment() {
+    private val sharedViewModel: OrderViewModel by activityViewModels()
 
     // Binding object instance corresponding to the fragment_start.xml layout
     // This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
@@ -47,16 +50,20 @@ class StartFragment : Fragment() {
 
         binding?.apply {
             // Set up the button click listeners
-            orderOneCupcake.setOnClickListener { orderCupcake() }
-            orderSixCupcakes.setOnClickListener { orderCupcake() }
-            orderTwelveCupcakes.setOnClickListener { orderCupcake() }
+            orderOneCupcake.setOnClickListener { orderCupcake(1) }
+            orderSixCupcakes.setOnClickListener { orderCupcake(6) }
+            orderTwelveCupcakes.setOnClickListener { orderCupcake(12) }
         }
     }
 
     /**
      * Start an order with the desired quantity of cupcakes and navigate to the next screen.
      */
-    fun orderCupcake() {
+    fun orderCupcake(quantity:Int) {
+        sharedViewModel.setQuantity(quantity)
+        if(sharedViewModel.hasNoFlavourSet()){
+            sharedViewModel.setFlavour(getString(R.string.vanilla))
+        }
         findNavController().navigate(R.id.action_startFragment_to_flavorFragment)
     }
 
--- app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -3,18 +3,20 @@ package com.example.cupcake.model
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.text.SimpleDateFormat
+import java.util.*
 
 class OrderViewModel: ViewModel() {
-    private val _quantity=MutableLiveData<Int>(0)
+    private val _quantity=MutableLiveData<Int>()
     val quantity:LiveData<Int> get() = _quantity
 
-    private val _flavor=MutableLiveData<String>("")
+    private val _flavor=MutableLiveData<String>()
     val flavour:LiveData<String> get() = _flavor
 
-    private val _date=MutableLiveData<String>("")
+    private val _date=MutableLiveData<String>()
     val date:LiveData<String>   get() = _date
 
-    private val _price=MutableLiveData<Double>(0.0)
+    private val _price=MutableLiveData<Double>()
     val price:LiveData<Double>  get() = _price
 
     fun setQuantity(numOfCupCakes:Int){
@@ -25,5 +27,33 @@ class OrderViewModel: ViewModel() {
     }
     fun setDate(pickedDate:String){
         _date.value=pickedDate
+    }
+
+    val dateOptions=getPickupOptions()
+
+    init {
+        resetOrder()
+    }
+
+
+    fun hasNoFlavourSet():Boolean{
+        return _flavor.value.isNullOrEmpty()
+    }
+    private fun getPickupOptions(): List<String> {
+        val options = mutableListOf<String>()
+        val formatter = SimpleDateFormat("E MMM d", Locale.getDefault())
+        val calendar = Calendar.getInstance()
+        repeat(4) {
+            options.add(formatter.format(calendar.time))
+            calendar.add(Calendar.DATE, 1)
+        }
+        return options
+    }
+
+    fun resetOrder() {
+        _quantity.value = 0
+        _flavor.value = ""
+        _date.value = dateOptions[0]
+        _price.value = 0.0
     }
 }
