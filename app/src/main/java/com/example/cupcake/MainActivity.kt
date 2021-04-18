2996e170d04e883a810aebca2ea929d66b2487ac
nikhi <nikhilshetye26@gmail.com>
2021-04-18 08:26:34 +0530

    Initial Ui implemented

--- app/src/main/java/com/example/cupcake/MainActivity.kt
+++ app/src/main/java/com/example/cupcake/MainActivity.kt
@@ -15,9 +15,31 @@
  */
 package com.example.cupcake
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 /**
  * Activity for cupcake order flow.
  */
 class MainActivity : AppCompatActivity(R.layout.activity_main)//main
+{
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()||super.onSupportNavigateUp()
+    }
+}
